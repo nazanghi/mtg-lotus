dc61a692abce3680e48b6a844b953da8690460f8
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-15 15:07:50 -0500

    makes demo Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,10 +4,62 @@ import Search from '../components/TextInput'
 
 export default () => {
     return (
-        <div></div>
+        <div className="home content-wrapper flex-col">
+            <div className="content flex-col">
+                <form>
+                    <Search 
+                        name="search" 
+                        type="search"
+                        placeholder="Look for a Card"
+                    />
+                </form>
+                <div className="discover flex-col">
+                    <h2>Discover</h2>
+                    <section className="card-wrapper flex-row">
+                        <Card>
+                            <div className="mask flex-col">
+                                <div className="card-content">
+                                    <h3>Testing it out</h3>
+                                    <p> with some text here too </p>
+                                </div>
+                            </div>
+                            <img 
+                                src=""
+                                alt=""
+                            />
+                        </Card>
+                        <Card>
+                            <div className="mask flex-col">
+                                <div className="card-content">
+                                    <h3>Testing it out2</h3>
+                                    <p> with some text here too </p>
+                                </div>
+                            </div>
+                            <img 
+                                src=""
+                                alt=""
+                            />
+                        </Card>
+                        <Card>
+                            <div className="mask flex-col">
+                                <div className="card-content">
+                                    <h3>Testing it out3</h3>
+                                    <p> with some text here too </p>
+                                </div>
+                            </div>
+                            <img 
+                                src=""
+                                alt=""
+                            />
+                        </Card>
+                    </section>
+                </div>
+            </div>
+        </div>
     )
 }
 
 //This whole page needs to be fleshed out but realistically
 //I think it's the last thing I'll do because I've got the otehr stuff that needs 
 //to be worked out
+//for now this is a working thing that I can visually see and adjust 
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -5,8 +5,32 @@ import Nav from '../components/Nav' //gotta make a Nav component
 
 export default ({ children }) => {
     return( //gotta add a bunch of divs and match em up with CSS
-        <div>
-            <Hero />
+        <div className = "landing-page flex-row">
+            <section className="left flex-sm flex-col">
+                <div className ="mask flex-col">
+                    <div className="content-wrapper flex-row">
+                        <h3 className="logo">Logo</h3>
+                        <div className="hero-wrapper flex-row">
+                        <div className ="cl-left flex-col">
+                            <Hero />        
+                        </div>
+                        <div className="cl-right flex-col">
+                            <div className="path-wrapper">
+                                <img src={} alt =""/>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+            <div className="image-wrapper">
+                <img src={} alt=""/>
+            </div>
+            </section>
+            <section className = "right flex-sm">
+                <Nav/>
+                {children}
+            </section>
         </div>
     )
 }

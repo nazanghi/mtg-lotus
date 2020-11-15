304a2c326ce80c9e019ef43e04f5de691795def1
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-14 20:19:07 -0500

    adds user services

---
+++ frontend/src/services/UserServices.js
@@ -0,0 +1,30 @@
+import ApiClient from './ApiClient'
+
+export const __GetProfile = async (userId) => {
+    try {
+        const response = await ApiClient.get(`/users/${userId}`)
+        return response.data
+    } catch (error) {throw error}
+}
+
+export const __RegisterUser = async (formData) => {
+    try {
+        const response = await ApiClient.get('/users/register', formData)
+    } catch (error) {throw error}
+}
+
+export const __CheckSession = async () => {
+    try {
+        const response = await ApiClient.get('/users/refresh/session')
+        console.log(response.data)
+        return response.data
+    } catch (error) {throw error}
+}
+
+export const __LoginUser = async (userData) => {
+    try {
+        const response = await ApiClient.post('/users/login', userData)
+        localStorage.setItem('token', response.data.token)
+        return response.data
+    } catch (error) {throw error}
+}
